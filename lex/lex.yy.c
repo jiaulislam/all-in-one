e1945c4649b07bd7443c57b011fe6349f96d711c
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-04-09 20:27:25 +0600

    solved

--- lex/lex.yy.c
+++ lex/lex.yy.c
@@ -284,9 +284,9 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 
 #define YY_NUM_RULES 3
 #define YY_END_OF_BUFFER 4
-static yyconst short int yy_accept[9] =
+static yyconst short int yy_accept[10] =
     {   0,
-        0,    0,    4,    2,    3,    1,    1,    0
+        1,    1,    4,    2,    3,    1,    2,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -323,27 +323,27 @@ static yyconst int yy_ec[256] =
 
 static yyconst int yy_meta[4] =
     {   0,
-        1,    1,    2
+        1,    2,    1
     } ;
 
-static yyconst short int yy_base[10] =
+static yyconst short int yy_base[11] =
     {   0,
-        0,    0,    5,    6,    6,    0,    0,    6,    2
+        0,    0,    6,    0,    7,    2,    0,    0,    7,    3
     } ;
 
-static yyconst short int yy_def[10] =
+static yyconst short int yy_def[11] =
     {   0,
-        8,    1,    8,    8,    8,    9,    9,    0,    8
+        9,    1,    9,   10,    9,   10,   10,    6,    0,    9
     } ;
 
-static yyconst short int yy_nxt[10] =
+static yyconst short int yy_nxt[11] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8
+        4,    5,    6,    7,    8,    9,    3,    9,    9,    9
     } ;
 
-static yyconst short int yy_chk[10] =
+static yyconst short int yy_chk[11] =
     {   0,
-        1,    1,    1,    9,    3,    8,    8,    8,    8
+        1,    1,    1,   10,    6,    3,    9,    9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -357,12 +357,14 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 ".\\problem2.l"
+#line 1 ".\\problem3.l"
 #define INITIAL 0
-#line 2 ".\\problem2.l"
+#line 2 ".\\problem3.l"
 #include <string.h>
 char s[10000];
-#line 366 "lex.yy.c"
+int i;
+int last_digit_of_my_id = 3;
+#line 368 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -513,10 +515,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 6 ".\\problem2.l"
+#line 7 ".\\problem3.l"
 
-
-#line 520 "lex.yy.c"
+#line 521 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -567,13 +568,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -601,20 +602,35 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 ".\\problem2.l"
-{printf("%s", yytext) ;printf("\n%s",yytext+4);}
+#line 8 ".\\problem3.l"
+{  
+    int ctr = 0;
+    printf("Input: %s\n", yytext);
+	strcpy(s,yytext);
+	for(i=0; i<strlen(s); i++)
+	{
+		if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || 
+            s[i]=='u')
+		    ctr++;
+	}
+	if (ctr){
+    	printf("Output: %d %s\n", ctr, yytext+last_digit_of_my_id);
+	}else{
+    	printf("Output: %d\n", ctr);
+	}
+}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 9 ".\\problem2.l"
+#line 24 ".\\problem3.l"
 
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 11 ".\\problem2.l"
+#line 25 ".\\problem3.l"
 ECHO;
 	YY_BREAK
-#line 618 "lex.yy.c"
+#line 634 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -906,7 +922,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -941,11 +957,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 9);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1500,7 +1516,7 @@ int main()
 	return 0;
 	}
 #endif
-#line 11 ".\\problem2.l"
+#line 25 ".\\problem3.l"
 
 
 int yywrap()
